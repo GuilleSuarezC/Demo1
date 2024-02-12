0d426c6cb8b5a8c1d316e1dcdb46bec8688ce48b
GUILLE <GUILLE@DESKTOP-9JMIMCN>
2024-02-12 22:56:56 +0100

    Changes on exercise3

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -4,9 +4,9 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Línea 1");
-		System.out.println("Línea 2");
-		System.out.println("Línea 3");
+		System.out.println("Línea REBASE");
+		System.out.println("Línea REBASE");
+		System.out.println("Línea REBASE");
 		System.out.println("Mods on master.");
 		System.err.print("Mods Master.");
 		System.err.print("Mods on masteren't");
